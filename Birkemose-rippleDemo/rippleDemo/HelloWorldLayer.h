1405aba237fc6ff2a7ca085bf28171bec091b71d
Mingle Chang <mingle890805@gmail.com>
2013-02-19 10:33:09 +0800

    Birkemose-rippleDemo
    create

---
+++ Birkemose-rippleDemo/rippleDemo/HelloWorldLayer.h
@@ -0,0 +1,22 @@
+//
+//  HelloWorldLayer.h
+//  rippleDemo
+//
+//  Created by Lars Birkemose on 02/12/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+#import "pgeRippleSprite.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer {
+    pgeRippleSprite* rippleImage;
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
